908a5862b6e98bcd972a0edc733cd5743edecbea
DavidCDCB <cristian-cb1@hotmail.com>
2022-01-05 13:45:57 -0500

    Prueba Deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./config";
 import app from './app';
 import "./database";
 
-
 app.listen(process.env.PORT || 3000,()=>{
 	console.log(`Server running on ${process.env.IP || "localhost"}:${process.env.PORT || 3000}`);
 });
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,10 @@ import userController from '../controllers/user.controller';
 
 const router = new Router();
 
+router.get("/", (req, res) => {
+	res.send("OK");
+});
+
 router.get("/api/users", userController.getUsers);
 router.get("/api/user/:id", userController.getUser);
 router.post("/api/user", userController.saveUser);
